f87764f3a0fa073f815cca45d6d05e6e17f32813
Arif Hossain <hossaininfo@ymail.com>
2023-01-29 22:33:15 +0100

    created auth guard so if user remove data from session storage appllication redirect to the main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,28 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginPage} from "./pages/login/login.page";
 import {GuitarCataloguePage} from "./pages/guitar-catalogue/guitar-catalogue.page";
 import {ProfilePage} from "./pages/profile/profile.page";
+import {AuthGuard} from "./guards/auth.guard";
 
 
 const routes: Routes = [
   {
-    path:"",
-    pathMatch:"full",
-    redirectTo:"login"
+    path: "",
+    pathMatch: "full",
+    redirectTo: "login"
   },
   {
     path: "login",
     component: LoginPage
   },
   {
-    path:"guitars",
-    component:GuitarCataloguePage
+    path: "guitars",
+    component: GuitarCataloguePage,
+    canActivate: [AuthGuard]
   },
   {
-    path:"profile",
-    component:ProfilePage
+    path: "profile",
+    component: ProfilePage,
+    canActivate: [AuthGuard],
   }
 ]
 

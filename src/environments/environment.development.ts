e489c33385f6bae201fc19288357174371d7b130
Arif Hossain <hossaininfo@ymail.com>
2023-01-30 13:22:09 +0100

    removed environment file from git history

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "aölksdjfkajdfajdklfjalkdfj",
-  apiUsers: "https://noroff-assignment-api-production-741f.up.railway.app/users",
-  apiGuitars: "https://noroff-assignment-api-production-741f.up.railway.app/guitars",
-};

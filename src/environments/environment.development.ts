11740c58bfc2dde3b4a616def35370d3b57c2606
Arif Hossain <hossaininfo@ymail.com>
2023-02-09 21:36:50 +0100

    added nav bar

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiKey: "aölksdjfkajdfajdklfjalkdfj",
+  apiUsers: "https://noroff-assignment-api-production-741f.up.railway.app/users",
+  apiGuitars: "https://noroff-assignment-api-production-741f.up.railway.app/guitars",
+};
